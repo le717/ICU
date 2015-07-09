92f4ab32607ca1fbbaebe3d4e555fef7b68dd9ea
Triangle717 <le717.code@yahoo.com>
2015-07-09 17:43:49 -0400

    Add queue module, which will be greatly expanded

---
+++ src/core/queue.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""ICU (LEGO Island Configuration Utility).
+
+Created 2015 Triangle717
+<http://le717.github.io/>
+
+Licensed under The MIT License
+<http://opensource.org/licenses/MIT/>
+
+"""
+
+
+class ActionsQueue:
+
+    def __init__(self):
+        self.queue = []
+
+    # Normal buttons
+    def btnBrowse(self, val):
+        print(val)
+
+    def actionReset(self, val):
+        print(val)
+
+    def btnRedirect(self, val):
+        print(val)
+
+    # Radio buttons
+    def radioColor256(self, val):
+        print(val)
+
+    def radioColor16b(self, val):
+        print(val)
+
+    def radioModelLow(self, val):
+        print(val)
+
+    def radioModelFast(self, val):
+        print(val)
+
+    def radioModelHigh(self, val):
+        print(val)
+
+    def radioTexFast(self, val):
+        print(val)
+
+    def radioTexHigh(self, val):
+        print(val)
+
+    # Check boxes
+    def chkCursor(self, val):
+        print(val)
+
+    def chkDraw3D(self, val):
+        print(val)
+
+    def chkFlipVideo(self, val):
+        print(val)
+
+    def chkFullscreen(self, val):
+        print(val)
+
+    def chkJoystick(self, val):
+        print(val)
+
+    def chkMusic(self, val):
+        print(val)
+
+    def chkSound(self, val):
+        print(val)
+
+    # Direct3D dropdown selection
+    def comboD3D(self, val):
+        print(val)
