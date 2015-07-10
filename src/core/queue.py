cf69cfa65d2a25149eb29890dad147ae3f2ab2f9
Triangle717 <le717.code@yahoo.com>
2015-07-09 21:16:30 -0400

    Fill in small part of responses
    Add blueprint of what queue should be like

--- src/core/queue.py
+++ src/core/queue.py
@@ -15,66 +15,87 @@ class ActionsQueue:
     def __init__(self):
         self.queue = []
 
+        # Check if group of vals or just one
+            # Perform routine in loop if needed
+
+        # Check type for presence in queue, flag result & index
+
+        # Check if val == registry from onload cache
+            # if true return
+
+        # Check if already present in queue
+            # If true and val !== queue
+                    # Update index with new val
+            # Else if false
+                    # Add val to queue at end
+
 
 class Responses:
 
     def __init__(self):
         pass
 
-    # Normal buttons
-    def btnBrowse(self, val):
-        print(val)
+    # Mass actions
+    def actionApply(self, val):
+        print("", val)
 
     def actionReset(self, val):
-        print(val)
+        print("", val)
+
+    # Normal buttons
+    def btnBrowse(self, val):
+        print("", val)
 
     def btnRedirect(self, val):
-        print(val)
+        print("", val)
 
     # Radio buttons
     def radioColor256(self, val):
-        print(val)
+        print("color", "8")
 
     def radioColor16b(self, val):
-        print(val)
+        print("color", "16")
 
     def radioModelLow(self, val):
-        print(val)
+        print("model", "0")
 
     def radioModelFast(self, val):
-        print(val)
+        print("model", "1")
 
     def radioModelHigh(self, val):
-        print(val)
+        print("model", "2")
 
     def radioTexFast(self, val):
-        print(val)
+        print("texture", "0")
 
     def radioTexHigh(self, val):
-        print(val)
+        print("texture", "1")
 
     # Check boxes
     def chkCursor(self, val):
-        print(val)
+        print("cursor", val)
 
     def chkDraw3D(self, val):
-        print(val)
+        print("draw3d", val)
 
-    def chkFlipVideo(self, val):
-        print(val)
+    def chkFlipSurface(self, val):
+        print("surface", val)
 
-    def chkFullscreen(self, val):
-        print(val)
+    def chkWindowed(self, val):
+        print("windowed", val)
 
     def chkJoystick(self, val):
-        print(val)
+        print("joystick", val)
 
     def chkMusic(self, val):
-        print(val)
+        print("music", val)
 
     def chkSound(self, val):
-        print(val)
+        print("sound", val)
+
+    def chkWideAngle(self, val):
+        print("wideangle", val)
 
     # Direct3D dropdown selection
     def comboD3D(self, val):
-        print(val)
+        print("d3d", val)
